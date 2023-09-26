b047f503933d10d71febbf4f7f8787c544d8d4bf
tanvir <tzather@gmail.com>
2023-09-26 12:20:13 -0600

    k8s

--- kubernetes/complex/run.sh
+++ kubernetes/complex/run.sh
@@ -4,3 +4,7 @@ docker compose -f docker-compose-dev.yml down
 
 # apply all files in the k8s directory
 kubectl apply -f k8s
+
+# create secret
+kubectl create secret generic pgpassword --from-literal PGPASSWORD=12345asdf
+kubectl get secret
